2294c4deda1375691e16850809e84f39fe462a2e
mddarr <mddarr@gmail.com>
2021-01-17 23:06:13 -0800

    insert a few providers in the sql flyway

---
+++ relational_microservices/healthcare-service/src/main/resources/db/migration/V2__ddl.sql
@@ -0,0 +1,2 @@
+INSERT INTO providers.providers(providerid, first_name, last_name, title, department) VALUES('provider1','Charles','Dickens','Doctor','Neurology');
+INSERT INTO providers.providers(providerid, first_name, last_name, title, department) VALUES('provider2','Erik','Williams','Doctor','Rheumatology');
